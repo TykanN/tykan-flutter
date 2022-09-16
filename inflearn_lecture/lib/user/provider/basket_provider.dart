4f85764bfdf2f875a959ab5b01b0b3329b12e1f9
Theo / Taeyoon Kang <kang3603a@gmail.com>
2022-09-16 19:33:20 +0900

    day-7

--- inflearn_lecture/lib/user/provider/basket_provider.dart
+++ inflearn_lecture/lib/user/provider/basket_provider.dart
@@ -1,15 +1,40 @@
+import 'package:collection/collection.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import 'package:inflearn_lecture/product/model/product_model.dart';
 import 'package:inflearn_lecture/user/model/basket_item_model.dart';
-import 'package:collection/collection.dart';
+import 'package:inflearn_lecture/user/model/patch_basket_body.dart';
+import 'package:inflearn_lecture/user/repository/user_me_repository.dart';
 
 final basketProvider = StateNotifierProvider<BasketNotifier, List<BasketItemModel>>((ref) {
-  return BasketNotifier();
+  final repo = ref.watch(userMeRepositoryProvider);
+  return BasketNotifier(repository: repo);
 });
 
 class BasketNotifier extends StateNotifier<List<BasketItemModel>> {
-  BasketNotifier() : super([]);
+  final UserMeRepository repository;
 
+  BasketNotifier({
+    required this.repository,
+  }) : super([]);
+
+  Future<void> patchBasket() async {
+    await repository.patchBasket(
+      body: PatchBasketBody(
+        basket: state
+            .map(
+              (e) => PatchBasketBodyBasket(productId: e.product.id, count: e.count),
+            )
+            .toList(),
+      ),
+    );
+  }
+
+  // <기존 방식>
+  // 요청을 먼저 보내고 응답이 오면 캐시를 업데이트
+  // <개선 방식>
+  // 먼저 반영을 하고, 요청을 보낸다
+  // Optimistic Response (긍정적 응답) --> 응답이 성공할거라 가정하고 상태를 먼저 업데이트
   Future<void> addToBasket({required ProductModel product}) async {
     final exists = state.firstWhereOrNull((e) => e.product.id == product.id) != null;
     // 1) 이미 있다면 장바구니에 있는 값에 카운트 증가
@@ -28,6 +53,8 @@ class BasketNotifier extends StateNotifier<List<BasketItemModel>> {
         BasketItemModel(product: product, count: 1),
       ];
     }
+
+    await patchBasket();
   }
 
   Future<void> removeFromBasket({
@@ -50,5 +77,7 @@ class BasketNotifier extends StateNotifier<List<BasketItemModel>> {
     } else {
       state = state.map((e) => e.product.id == product.id ? e.copyWith(count: e.count + 1) : e).toList();
     }
+
+    await patchBasket();
   }
 }
